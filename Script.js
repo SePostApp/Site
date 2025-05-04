5265f262d82f7fd580f36a79e1dfb21aef9446fa
SePostApp <riorizky777@gmail.com>
2025-05-04 16:26:52 +0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,39 @@
+function startNow() {
+  window.location.href = "https://app.nodepay.ai/register";
+}
+
+    function toggleMenu() {
+      document.getElementById("navbar").classList.toggle("nav-open");
+    }
+
+particlesJS('particles-js1', {
+  particles: {
+    number: { value: 800 },
+    size: { value: 1.0, random : true},
+    color: { value: "#ffffff" },
+    opacity: { value: 0.5 },
+    line_linked: { enable: false, opacity: 0.5},
+    move: { speed: 0.9 }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: { onhover: { enable: false }, onclick: { enable: false }}
+  },
+  retina_detect: true
+});
+
+particlesJS('particles-js2', {
+  particles: {
+    number: { value: 500 },
+    size: { value: 0.5, random : true},
+    color: { value: "#ffffff" },
+    opacity: { value: 0.5 },
+    line_linked: { enable: true, opacity: 0.5},
+    move: { speed: 0.5 }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: { onhover: { enable: false }, onclick: { enable: false }}
+  },
+  retina_detect: true
+});
